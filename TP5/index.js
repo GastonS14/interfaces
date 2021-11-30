37dd8b9fb1239797fee0e00bbce275bca014f78c
Juan Segundo Castaño Rossi <juansec@live.com.ar>
2021-11-30 19:45:21 -0300

    adding profile, chat & fixing home and logIn

---
+++ TP5/index.js
@@ -0,0 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", cargarPagina); 
+
+function cargarPagina() { 
+    document.getElementById("signIn").addEventListener("click", goToHome );
+    document.getElementById("user").addEventListener("click", clearError );
+    document.getElementById("pass").addEventListener("click", clearError );
+}
+
+function goToHome() {
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    if ( user === "juanCarlos") {
+        if ( pass == 1234 )
+            window.location.href = getPath() + "home.html"; 
+        else{
+            document.getElementById("wrongPass").classList.remove("dontShow");
+            document.getElementById("wrongPass").classList.add("show");
+        }
+    } else {
+        let smal = document.getElementById("wrongUser").classList.remove("dontShow") 
+        document.getElementById("wrongUser").classList.add("show");
+    }
+}
+
+function getPath (){
+    var loc = window.location;
+    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+}
+
+function clearError () { 
+    document.getElementById("wrongPass").classList.add("dontShow");
+    document.getElementById("wrongPass").classList.remove("show");
+    document.getElementById("wrongUser").classList.add("dontShow");
+    document.getElementById("wrongUser").classList.remove("show");
+}
