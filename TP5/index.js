91427fa0ed0c8f1fbefeeba34fb4fd8be8a0e4a9
Juan Segundo Castaño Rossi <juansec@live.com.ar>
2021-12-01 15:34:30 -0300

    adding promotion in posts

--- TP5/index.js
+++ TP5/index.js
@@ -18,14 +18,14 @@ function goToHome() {
             document.getElementById("wrongPass").classList.add("show");
         }
     } else {
-        let smal = document.getElementById("wrongUser").classList.remove("dontShow") 
+        document.getElementById("wrongUser").classList.remove("dontShow") 
         document.getElementById("wrongUser").classList.add("show");
     }
 }
 
 function getPath (){
-    var loc = window.location;
-    var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+    let loc = window.location;
+    let pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
     return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
 }
 
